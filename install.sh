dc1ef71b5b60347032e1b536ebe8de089efa314a
teyo <teyo.13324@gmail.com>
2020-04-14 09:51:15 +0900

    create install.sh as trial

---
+++ install.sh
@@ -0,0 +1,11 @@
+wget https://raw.githubusercontent.com/YoheiSaito/MyPCLTemplate/master/pcl_builder -O pcl_builder
+chmod +x pcl_builder
+$install_path=$HOME/.local/bin/
+if [ -d $install_path ]; then
+  mv pcl_builder $install_path
+else
+  mkdir -p $install_path
+  mv pcl_builder $install_path
+  PATH=$PATH:$(echo $install_path)
+  echo "export PATH=$PATH:$(echo $install_path)" >> $HOME/.profile
+fi
