c2c656dfec18419d8173ac73f6cf0572ea90267e
teyo <teyo.13324@gmail.com>
2020-04-14 09:56:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 wget https://raw.githubusercontent.com/YoheiSaito/MyPCLTemplate/master/pcl_builder -O pcl_builder
 chmod +x pcl_builder
-$install_path=$HOME/.local/bin/
+install_path=$HOME/.local/bin/
 if [[ -d $install_path ]]; then
   mv pcl_builder $install_path
 else
-  mkdir -p $install_path
+  mkdir https://raw.githubusercontent.com/YoheiSaito/MyPCLTemplate/master/install.sh-p $install_path
   mv pcl_builder $install_path
   PATH=$PATH:$(echo $install_path)
   echo "export PATH=$PATH:$(echo $install_path)" >> $HOME/.profile
